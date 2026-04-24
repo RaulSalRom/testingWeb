04cc115d65ccf23b4abad1e31a735c4eb5acc70c
jarvisclaw-dev <supercellraulsal@gmail.com>
2026-04-24 09:14:23 +0000

    build: update API URL to tunnel endpoint and rebuild frontend

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "/hcgi/platform";
+const POCKETBASE_API_URL = "https://corina-server.tail4f61af.ts.net";
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 

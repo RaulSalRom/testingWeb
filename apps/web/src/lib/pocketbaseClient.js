986842386c1e9791cde11f34e26648b2af9eefbb
jarvisclaw-dev <supercellraulsal@gmail.com>
2026-04-24 10:01:32 +0000

    clean: repo with only dist/apps/web/ for Hostinger deployment

--- apps/web/src/lib/pocketbaseClient.js
+++
@@ -1,9 +0,0 @@
-import Pocketbase from 'pocketbase';
-
-const POCKETBASE_API_URL = "https://corina-server.tail4f61af.ts.net";
-
-const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
-
-export default pocketbaseClient;
-
-export { pocketbaseClient };
